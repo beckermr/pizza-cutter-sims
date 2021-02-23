1dfd351754a9d1a8a07542a63d832ca7946c3220
Matthew R. Becker <becker.mr@gmail.com>
2021-02-23 17:50:29 -0600

    WIP run pizza cutter

--- pizza_cutter_sims/tests/test_sim.py
+++ pizza_cutter_sims/tests/test_sim.py
@@ -6,57 +6,31 @@ from pizza_cutter_sims.tests.conftest import recursive_equal
 from pizza_cutter_sims.sim import generate_sim
 
 
+def _run_sim(cfg, rng_seed, gal_rng_seed):
+    cfg["shear"]["g1"] = 0.0
+    cfg["shear"]["g2"] = 0.0
+
+    rng = np.random.RandomState(seed=rng_seed)
+    gal_rng = np.random.RandomState(seed=gal_rng_seed)
+    return generate_sim(
+        rng=rng,
+        gal_rng=gal_rng,
+        coadd_config=cfg["coadd"],
+        se_config=cfg["se"],
+        psf_config=cfg["psf"],
+        gal_config=cfg["gal"],
+        layout_config=cfg["layout"],
+        msk_config=cfg["msk"],
+        shear_config=cfg["shear"],
+    )
+
+
 def test_generate_sim_seeding(sim_config):
-    cfg = copy.deepcopy(sim_config)
-    cfg["shear"]["g1"] = 0.0
-    cfg["shear"]["g2"] = 0.0
-    rng = np.random.RandomState(seed=42)
-    gal_rng = np.random.RandomState(seed=42)
-    sdata1 = generate_sim(
-        rng=rng,
-        gal_rng=gal_rng,
-        coadd_config=cfg["coadd"],
-        se_config=cfg["se"],
-        psf_config=cfg["psf"],
-        gal_config=cfg["gal"],
-        layout_config=cfg["layout"],
-        msk_config=cfg["msk"],
-        shear_config=cfg["shear"],
-    )
-
-    cfg = copy.deepcopy(sim_config)
-    cfg["shear"]["g1"] = 0.0
-    cfg["shear"]["g2"] = 0.0
-    rng = np.random.RandomState(seed=42)
-    gal_rng = np.random.RandomState(seed=42)
-    sdata2 = generate_sim(
-        rng=rng,
-        gal_rng=gal_rng,
-        coadd_config=cfg["coadd"],
-        se_config=cfg["se"],
-        psf_config=cfg["psf"],
-        gal_config=cfg["gal"],
-        layout_config=cfg["layout"],
-        msk_config=cfg["msk"],
-        shear_config=cfg["shear"],
-    )
-
-    cfg = copy.deepcopy(sim_config)
-    cfg["shear"]["g1"] = 0.0
-    cfg["shear"]["g2"] = 0.0
-    rng = np.random.RandomState(seed=4)
-    gal_rng = np.random.RandomState(seed=42)
-    sdata3 = generate_sim(
-        rng=rng,
-        gal_rng=gal_rng,
-        coadd_config=cfg["coadd"],
-        se_config=cfg["se"],
-        psf_config=cfg["psf"],
-        gal_config=cfg["gal"],
-        layout_config=cfg["layout"],
-        msk_config=cfg["msk"],
-        shear_config=cfg["shear"],
-    )
+    sdata1 = _run_sim(copy.deepcopy(sim_config), 42, 43)
+    sdata2 = _run_sim(copy.deepcopy(sim_config), 42, 43)
 
     assert recursive_equal(sdata1, sdata2)
-    assert not recursive_equal(sdata1, sdata3)
+
+    for seed_set in [(52, 43), (42, 53)]:
+        sdata3 = _run_sim(copy.deepcopy(sim_config), *seed_set)
+        assert not recursive_equal(sdata1, sdata3)
