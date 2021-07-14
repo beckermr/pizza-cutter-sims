be2030c8765675dce1ff806c50e78f5f370bb163
Matthew R. Becker <becker.mr@gmail.com>
2021-07-14 15:04:51 -0500

    TST fix more tests

--- pizza_cutter_sims/tests/test_sim.py
+++ pizza_cutter_sims/tests/test_sim.py
@@ -8,12 +8,13 @@ from pizza_cutter_sims.tests.conftest import recursive_equal
 from pizza_cutter_sims.sim import generate_sim
 
 
-def _run_sim(cfg, rng_seed, gal_rng_seed):
+def _run_sim(cfg, rng_seed, gal_rng_seed, star_rng_seed):
     cfg["shear"]["g1"] = 0.0
     cfg["shear"]["g2"] = 0.0
 
     rng = np.random.RandomState(seed=rng_seed)
     gal_rng = np.random.RandomState(seed=gal_rng_seed)
+    star_rng = np.random.RandomState(seed=star_rng_seed)
     return generate_sim(
         rng=rng,
         gal_rng=gal_rng,
@@ -24,16 +25,18 @@ def _run_sim(cfg, rng_seed, gal_rng_seed):
         layout_config=cfg["layout"],
         msk_config=cfg["msk"],
         shear_config=cfg["shear"],
+        star_config=cfg["star"],
+        star_rng=star_rng,
     )
 
 
 def test_generate_sim_seeding(sim_config):
-    sdata1 = _run_sim(copy.deepcopy(sim_config), 42, 43)
-    sdata2 = _run_sim(copy.deepcopy(sim_config), 42, 43)
+    sdata1 = _run_sim(copy.deepcopy(sim_config), 42, 43, 45)
+    sdata2 = _run_sim(copy.deepcopy(sim_config), 42, 43, 45)
 
     assert recursive_equal(sdata1, sdata2)
 
-    for seed_set in [(52, 43), (42, 53)]:
+    for seed_set in [(52, 43, 34), (42, 53, 67)]:
         sdata3 = _run_sim(copy.deepcopy(sim_config), *seed_set)
         assert not recursive_equal(sdata1, sdata3)
 
@@ -45,9 +48,9 @@ def test_generate_sim_seeding(sim_config):
     ("shear_std", 0.3),
 ])
 def test_generate_sim_wcs_changes(sim_config, key, val):
-    sdata1 = _run_sim(copy.deepcopy(sim_config), 42, 43)
+    sdata1 = _run_sim(copy.deepcopy(sim_config), 42, 43, 45)
     sconfig = copy.deepcopy(sim_config)
     sconfig["se"]["wcs_config"][key] = val
-    sdata2 = _run_sim(sconfig, 42, 43)
+    sdata2 = _run_sim(sconfig, 42, 43, 45)
 
     assert not recursive_equal(sdata1, sdata2)
